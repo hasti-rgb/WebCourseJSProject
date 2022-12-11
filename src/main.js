0bef82b23144fe63a0046a829dbad7db5fbb45df
Hasti Maleki <hasti.ml19@gmail.com>
2022-12-12 02:20:47 +0330

    add bonus feature

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 let shop = document.getElementById('items')
+let searchButton = document.getElementById('search-btn')
+let pageLogo = document.getElementById('logo')
+let category = document.getElementById('select')
+let categoryValu = 'category'
 
-let generateGrid = () => {
-  return (shop.innerHTML = productList
+let generateGrid = (list) => {
+  return (shop.innerHTML = list
     .map((x) => {
       let { id, img, title, desc1, desc2, price } = x
       return `
-        <div class="col-lg-4 ">
+        <div class="col-xl-4 col-lg-5 col-md-6 col-sm-6 ">
             <div
               class="d-flex flex-column justify-content-center mb-4 border shadow"
             >
@@ -25,7 +29,7 @@ let generateGrid = () => {
                   class="d-flex flex-row justify-content-between align-items-center mt-4"
                 >
                   <p>price ${price}$</p>
-                  <button onclick="incrementCartAmount()" class="add-btn px-3 py-2">Add to cart</button>
+                  <button onclick="incrementCartAmount()" class="btn add-btn px-3 py-2">Add to cart</button>
                 </div>
               </div>
             </div>
@@ -34,9 +38,74 @@ let generateGrid = () => {
     })
     .join(''))
 }
-generateGrid()
+generateGrid(productList)
 
 let incrementCartAmount = () => {
   let cartIcon = document.getElementById('cartAmount')
   console.log(cartIcon.innerHTML++)
+}
+pageLogo.onclick = () => {
+  generateGrid(productList)
+}
+category.onchange = () => {
+  categoryValu = category.options[category.selectedIndex].innerHTML
+}
+let filteringTheList = () => {
+  let filteredList = []
+  switch (categoryValu) {
+    case 'Makeup cosmetics':
+      productList.forEach((item) => {
+        if (item.category === 'Makeup cosmetics') {
+          filteredList.push(item)
+        }
+      })
+      break
+    case 'Skin care cosmetics':
+      productList.forEach((item) => {
+        if (item.category === 'Skin care cosmetics') {
+          filteredList.push(item)
+        }
+      })
+
+      break
+    case 'Hair care products':
+      productList.forEach((item) => {
+        if (item.category === 'Hair care products') {
+          filteredList.push(item)
+        }
+      })
+
+      break
+    default:
+      return productList
+  }
+  return filteredList
+}
+searchButton.onclick = () => {
+  let searchValue = document
+    .getElementById('search-form')
+    .value.toLocaleLowerCase()
+  searchValue = searchValue.trim()
+  if (searchValue.length === 0) {
+    console.log('please enter somthing')
+  } else {
+    let matchProducts = []
+    filteringTheList().forEach((item) => {
+      let itemTitle = item.title.toLocaleLowerCase()
+      let itemDesc1 = item.desc1.toLocaleLowerCase()
+      let itemDesc2 = item.desc2.toLocaleLowerCase()
+      if (
+        itemTitle.includes(searchValue) ||
+        itemDesc1.includes(searchValue) ||
+        itemDesc2.includes(searchValue)
+      ) {
+        matchProducts.push(item)
+      }
+    })
+    if (matchProducts.length !== 0) {
+      generateGrid(matchProducts)
+    } else {
+      alert('Nothing Match!')
+    }
+  }
 }
--- src/products.js
+++ src/products.js
@@ -2,7 +2,8 @@ let productList = [
   {
     id: '1',
     img: '../images/alexandra-tran-jNPVlv8USYA-unsplash.jpg',
-    title: 'Title',
+    title: 'FRE',
+    category: 'Hair care products',
     desc1:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
     desc2:
@@ -12,7 +13,8 @@ let productList = [
   {
     id: '2',
     img: '../images/ashley-piszek-2avqIBH252Y-unsplash.jpg',
-    title: 'Title',
+    title: 'Mascara',
+    category: 'Makeup cosmetics',
     desc1:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
     desc2:
@@ -22,7 +24,8 @@ let productList = [
   {
     id: '3',
     img: '../images/lucas-santos-OLp9FmszAAI-unsplash.jpg',
-    title: 'Title',
+    title: 'KIKO',
+    category: 'Makeup cosmetics',
     desc1:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
     desc2:
@@ -32,9 +35,10 @@ let productList = [
   {
     id: '4',
     img: '../images/ashley-piszek-9XLxrws19Cw-unsplash.jpg',
-    title: 'Title',
+    title: 'Lip Gloss',
+    category: 'Makeup cosmetics',
     desc1:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+      'mas ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
     desc2:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
     price: 45,
@@ -42,7 +46,8 @@ let productList = [
   {
     id: '5',
     img: '../images/pmv-chamara-QgXnwGyrVDs-unsplash.jpg',
-    title: 'Title',
+    title: 'Clinic',
+    category: 'Skin care cosmetics',
     desc1:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
     desc2:
@@ -52,7 +57,74 @@ let productList = [
   {
     id: '6',
     img: '../images/evangeline-sarney-EyrjiJAwLjQ-unsplash.jpg',
-    title: 'Title',
+    title: 'Eyeshadow Palette',
+    category: 'Makeup cosmetics',
+    desc1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+    desc2:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
+    price: 45,
+  },
+  {
+    id: '7',
+    img: '../images/crystalweed-cannabis-sb_HyHYGfuI-unsplash.jpg',
+    title: 'cream',
+    category: 'Skin care cosmetics',
+    desc1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+    desc2:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
+    price: 45,
+  },
+  {
+    id: '8',
+    img: '../images/lina-verovaya-BibJjO4sYrI-unsplash.jpg',
+    title: 'cream',
+    category: 'Skin care cosmetics',
+    desc1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+    desc2:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
+    price: 45,
+  },
+  {
+    id: '9',
+    img: '../images/pmv-chamara-MEsWk-dZzlI-unsplash.jpg',
+    title: 'Hair serum',
+    category: 'Hair care products',
+    desc1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+    desc2:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
+    price: 45,
+  },
+  {
+    id: '10',
+    img: '../images/evangeline-sarney-NnsqpLjiA94-unsplash.jpg',
+    title: 'Lip Stick',
+    category: 'Makeup cosmetics',
+    desc1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+    desc2:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
+    price: 45,
+  },
+  {
+    id: '11',
+    img: '../images/element5-digital-ceWgSMd8rvQ-unsplash.jpg',
+    title: 'Makeup Package',
+    category: 'Makeup cosmetics',
+    desc1:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
+    desc2:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor id cumque obcaecati voluptatem nam.',
+    price: 45,
+  },
+  {
+    id: '12',
+    img: '../images/laura-chouette-zpt4D5x3XPc-unsplash.jpg',
+    title: 'Givenchy',
+    category: 'Makeup cosmetics',
     desc1:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam dolores voluptate ipsa quae aliquid voluptatum, rerum itaque asperiores facilis odio, libero.',
     desc2:
